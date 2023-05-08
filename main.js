00cd8148b26c407ef2c2615a2f65c75e5e3c5afa
mehdiHajri <mehdi.hajri83gmail.com>
2023-05-08 17:57:40 +0200

    in progress

---
+++ main.js
@@ -0,0 +1,31 @@
+var archive=[]
+function startResult(num){
+    document.getElementById("result").textContent+=num
+}
+
+function clearResult() {
+    document.getElementById('result').textContent = ''
+  }
+  function calculateResult() {
+    var result = eval(document.getElementById('result').textContent)
+    document.getElementById('result').textContent = result
+    archive.push(result)
+  }
+  function archivee(){
+  if(archive.length===0){
+    document.getElementById("result").textContent="empty archive"
+  }
+  for (var i=0;i<archive.length;i++){ 
+    if(i===0){
+        document.getElementById("result").textContent+="this is archive "+archive[i]
+     } 
+     else
+    {
+    document.getElementById("result").textContent+="/"+archive[i]
+  } 
+  }
+  
+  }
+
+
+  
